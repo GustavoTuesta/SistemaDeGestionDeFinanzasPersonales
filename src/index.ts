89a88789c2b65696b97defee137f8cc68b7aee73
GustavoTuesta <tuesta2006@gmail.com>
2026-07-23 16:25:59 -0500

    Correccion en el schema de prisma y en el package.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'
-import { LoginController } from './controllers/login/login-controller'
+import loginRoute from './routes/login-route'
+import registerRoute from './routes/register-route'
+
 const app = express()
 app.use(express.json()) //middleware que permite transformar la req.body de una peticion a un json
 
 const PORT = 3000
 
-app.post('/api/login', LoginController)
+app.use("/api", loginRoute)
+app.use("/api", registerRoute)
 
 app.listen(PORT, () =>{
     console.log(`http://localhost:${PORT}`)
