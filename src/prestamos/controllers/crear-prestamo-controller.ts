d1b8d4376b869c681313ddbffc1fc235e3c18132
GustavoTuesta <tuesta2006@gmail.com>
2026-08-07 21:48:55 -0500

    Implementacion del manejo de errores en la seccion de prestamos

--- src/prestamos/controllers/crear-prestamo-controller.ts
+++ src/prestamos/controllers/crear-prestamo-controller.ts
@@ -1,19 +1,16 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prestamoDto } from "../prestamos-dto";
 import { prestamoService} from "../services/crear-prestamo-service"
 
 export async function prestamoController(
     req: Request<{}, {}, prestamoDto>,
-    res: Response
+    res: Response,
+    next: NextFunction
 ): Promise<void> {
     try {
         const registrarPrestamo = await prestamoService(req.body);
         res.status(201).json({ registrarPrestamo })    
     } catch (error) {
-        res.status(400).json({
-            message: error instanceof Error
-            ? error.message
-            : 'No se pudo registrar el prestamo'
-        })
+        next(error);
     }
 }
--- src/prestamos/controllers/eliminar-prestamo-controller.ts
+++ src/prestamos/controllers/eliminar-prestamo-controller.ts
@@ -1,18 +1,14 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { eliminarRegistroPrestamoService } from "../services/eliminar-prestamos-service";
 import { validarNumero } from "../../shared/services/validar-numero-service";
 
-export async function eliminarRegistroPrestamoController(req: Request, res: Response) {
+export async function eliminarRegistroPrestamoController(req: Request, res: Response, next: NextFunction) {
     try {
         const id = Number(req.params.id);
         validarNumero(id, "id");
         const eliminarRegistro = await eliminarRegistroPrestamoService(id);
         res.status(200).json({ eliminarRegistro });    
     } catch (error) {
-        res.status(400).json({
-            message: error instanceof Error
-            ? error.message
-            : 'No se pudo eliminar el registro de prestamo'
-        });
+        next(error);
     }
 }
